7a9de7b5c4748a90a5afc17c33a0999fb29479ec
tdubuffet <dubuffet.thibault@gmail.com>
2016-06-16 16:04:52 +0200

    Override ProfileFormType FosUserBundle

---
+++ src/UserBundle/Form/ProfileFormType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace UserBundle\Form;
+
+use FOS\UserBundle\Form\Type\ProfileFormType as AbstractProfileFormType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProfileFormType extends AbstractProfileFormType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder->add('firstname');
+        $builder->add('lastname');
+    }
+
+    public function getName()
+    {
+        return 'app_form_profil';
+    }
+}
