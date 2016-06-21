c234e74547e65f94810036cabdaba3b505dd63ec
jc <jeancharleslegoff@gmail.com>
2016-06-21 14:49:32 +0200

    Add admin custom page to moderate products

--- src/AdminBundle/Controller/AdminController.php
+++ src/AdminBundle/Controller/AdminController.php
@@ -3,6 +3,10 @@
 namespace AdminBundle\Controller;
 
 use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
 
 class AdminController extends BaseAdminController
 {
@@ -19,5 +23,18 @@ class AdminController extends BaseAdminController
     public function preUpdateUserEntity($user)
     {
         $this->get('fos_user.user_manager')->updateUser($user, false);
+    }
+
+
+    /**
+     * @Route(path = "/admin/moderate", name = "admin_moderate")
+     * @Template("AdminBundle:Admin:moderate.html.twig")
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function moderateAction()
+    {
+        $repository = $this->getDoctrine()->getRepository('ProductBundle:Product');
+        $products = $repository->findByStatus(1);
+        return ['products' => $products];
     }
 }
