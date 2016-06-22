f6e6c6607fc3065dc28bf98748d8c7d519fb5d83
jc <jeancharleslegoff@gmail.com>
2016-06-22 10:23:02 +0200

    Add relation between categories and products

--- src/ProductBundle/Entity/Product.php
+++ src/ProductBundle/Entity/Product.php
@@ -48,6 +48,12 @@ class Product
     private $price;
 
     /**
+     * @ORM\ManyToOne(targetEntity="CategoryBundle\Entity\Category")
+     * @ORM\JoinColumn(name="category_id", referencedColumnName="id")
+     */
+    private $category;
+
+    /**
      * @ORM\ManyToOne(targetEntity="Currency")
      * @ORM\JoinColumn(name="currency_id", referencedColumnName="id")
      */
@@ -242,4 +248,28 @@ class Product
         return $this->name;
     }
 
+
+    /**
+     * Set category
+     *
+     * @param \CategoryBundle\Entity\Category $category
+     *
+     * @return Product
+     */
+    public function setCategory(\CategoryBundle\Entity\Category $category = null)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \CategoryBundle\Entity\Category
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
 }
