563fcd5692d4b3c3deedcfd7b34dae773f2fb614
François <maps_red@yahoo.fr>
2016-08-23 11:42:12 +0200

    Add comments message edition

--- src/CommentBundle/Controller/AdminController.php
+++ src/CommentBundle/Controller/AdminController.php
@@ -7,9 +7,11 @@ use CommentBundle\Event\CommentEvent;
 use CommentBundle\Event\CommentEvents;
 use CommentBundle\Form\CommentType;
 use ProductBundle\Entity\Product;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -17,7 +19,6 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class AdminController extends Controller
 {
-
     /**
      * @Route("/commentaires", name="admin_comments")
      * @Template("CommentBundle:Admin:list.html.twig")
@@ -26,16 +27,13 @@ class AdminController extends Controller
      */
     public function listAction(Request $request)
     {
-        $comments = $this->getDoctrine()->getRepository('CommentBundle:Comment')->findBy([
-            'isValidated'   => false,
-            'isDeleted'     => false
-        ]);
+        $comments = $this->getDoctrine()->getRepository('CommentBundle:Comment')
+            ->findBy(['isValidated' => false, 'isDeleted' => false]);
 
         if ($request->get('save-comment')) {
-            foreach($comments as $comment) {
+            foreach ($comments as $comment) {
 
-                if ($val = $request->get('comment_' . $comment->getId())) {
-
+                if ($val = $request->get('comment_'.$comment->getId())) {
                     if ($val = 1) {
                         $comment->setIsValidated(true);
                         $comment->setIsDeleted(false);
@@ -47,31 +45,66 @@ class AdminController extends Controller
                     }
 
                     if ($comment->getParent()) {
-                        $this->get('event_dispatcher')->dispatch(
-                            CommentEvents::PRODUCT_COMMENT_REPLY,
-                            new CommentEvent($comment)
-                        );
+                        $this->get('event_dispatcher')
+                            ->dispatch(CommentEvents::PRODUCT_COMMENT_REPLY, new CommentEvent($comment));
                     } else {
-                        $this->get('event_dispatcher')->dispatch(
-                            CommentEvents::PRODUCT_COMMENT,
-                            new CommentEvent($comment)
-                        );
+                        $this->get('event_dispatcher')
+                            ->dispatch(CommentEvents::PRODUCT_COMMENT, new CommentEvent($comment));
                     }
 
                     $this->getDoctrine()->getManager()->persist($comment);
-
                 }
-
             }
-
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('admin_comments');
         }
 
-        return [
-            'comments' => $comments
-        ];
+        return ['comments' => $comments,];
+    }
+
+    /**
+     * @Route("/commentaires-ajax", name="ajax_admin_comment", options={"expose": true})
+     * @param Request $request
+     * @Method("POST")
+     * @return JsonResponse
+     */
+    public function commentAction(Request $request)
+    {
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return new JsonResponse(['message' => 'You must be authentificated to update the comment.'], 401);
+        }
+
+        if (!$request->request->has('comment_id')) {
+            return new JsonResponse(['message' => 'comment_id parameter missing'], 409);
+        }
+        if (!in_array($request->request->get('action'), ['save', 'get'])) {
+            return new JsonResponse(['message' => 'action parameter not valid or missing'], 409);
+        }
+        $id = $request->request->get('comment_id');
+        $comment = $this->getDoctrine()->getRepository("CommentBundle:Comment")->findOneBy(['id' => $id]);
+
+        if (!$comment) {
+            return new JsonResponse(['message' => sprintf('No comment found for id %s', $id)], 404);
+        }
+
+        if ($request->request->get('action') == 'get') {
+            return new JsonResponse(['comment' => ['message' => $comment->getMessage(), 'id' => $comment->getId()]]);
+        }
+
+        if ($request->request->get('action') == 'save') {
+            if (!$request->request->has('comment_message')) {
+                return new JsonResponse(['message' => 'comment_message parameter missing'], 409);
+            }
+
+            $comment->setMessage($request->request->get('comment_message'));
+            $this->getDoctrine()->getManager()->persist($comment);
+            $this->getDoctrine()->getManager()->flush();
+
+            return new JsonResponse(['comment' => ['message' => $comment->getMessage(), 'id' => $comment->getId()]]);
+        }
+
+        return new JsonResponse(['message' => 'action parameter not valid'], 409);
     }
 
 }
---
+++ src/CommentBundle/Resources/public/js/comments.js
@@ -0,0 +1,61 @@
+/**
+ * Created by francois on 23/08/16.
+ */
+
+var Comments = {
+    init: function () {
+        $('#textEdit').on('shown.bs.modal', function (event) {
+            $("input[name='text']").val("");
+            $("input[name='id']").val("");
+            Comments.button = $(event.relatedTarget);
+            Comments.getComment();
+        });
+
+        $(".save-btn").click(function () {
+            Comments.saveComment();
+        })
+    },
+
+    getComment: function () {
+        $.ajax({
+            url: Routing.generate('ajax_admin_comment'),
+            type: 'POST',
+            data: {'comment_id': Comments.button.data('id'), 'action': 'get'},
+            success: function (result) {
+                var comment = result['comment'];
+                $("input[name='text']").val(comment.message);
+                $("input[name='id']").val(comment.id);
+            },
+            error: function (result) {
+                if (result.status == 401) {
+                    window.location.href = Routing.generate('fos_user_security_login');
+                }
+            }
+        });
+    },
+
+    saveComment: function () {
+        var id = $("input[name='id']").val();
+        $.ajax({
+            url: Routing.generate('ajax_admin_comment'),
+            type: 'POST',
+            data: {'comment_id': id, 'action': 'save', 'comment_message' : $("input[name='text']").val()},
+            success: function (result) {
+                var comment = result['comment'];
+                var child = $(Comments.button.parent('td')[0]).children('span')[0];
+                $(child).html(comment.message);
+                $('h1.title').after('<p class="text-success">Commentaire modifié</p>');
+
+            },
+            error: function (result) {
+                if (result.status == 401) {
+                    window.location.href = Routing.generate('fos_user_security_login');
+                }
+            }
+        });
+    }
+};
+
+$(document).ready(function () {
+    Comments.init();
+});
