e10adf8a0ba547151cd9714c35a721d57aba7873
tdubuffet <dubuffet.thibault@gmail.com>
2016-08-26 09:50:20 +0200

    Add referential ad2

--- src/Admin2Bundle/EventListener/MenuItemListListener.php
+++ src/Admin2Bundle/EventListener/MenuItemListListener.php
@@ -26,12 +26,12 @@ class MenuItemListListener {
         $menuItems = array(
             new MenuItemModel('moderation', 'Modération', 'ad2_moderation_list', array(/* options */), 'iconclasses fa fa-chevron-circle-down'),
             $user = new MenuItemModel('user', 'Utilisateurs', 'ad2_user_list', array(/* options */), 'iconclasses fa fa-plane'),
+            $ref = new MenuItemModel('ref', 'Référentiel', 'ad2_ref_list', array(/* options */), 'iconclasses fa fa-wrench '),
             $config = new MenuItemModel('config', 'Configurations', 'ad2_user_list', array(/* options */), 'iconclasses fa fa-wrench ')
         );
 
         $user->addChild(new MenuItemModel('user-list', 'Recherche & liste', 'ad2_user_list', array(), 'fa fa-user'));
         $user->addChild(new MenuItemModel('user-messages', 'Messages', 'ad2_user_list'));
-
 
 
         $config->addChild(new MenuItemModel('category', 'Catégories', 'ad2_categories_list'));
