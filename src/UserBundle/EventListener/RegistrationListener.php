87298d87512f3f1a35072f0fc8ca84d3dd4b5137
tdubuffet <dubuffet.thibault@gmail.com>
2016-09-15 12:58:53 +0200

    Redirect to deposit if user is pro

--- src/UserBundle/EventListener/RegistrationListener.php
+++ src/UserBundle/EventListener/RegistrationListener.php
@@ -2,6 +2,7 @@
 
 namespace UserBundle\EventListener;
 
+use FOS\UserBundle\Event\GetResponseUserEvent;
 use FOS\UserBundle\FOSUserEvents;
 use FOS\UserBundle\Event\FormEvent;
 use FOS\UserBundle\Event\FilterUserResponseEvent;
@@ -9,23 +10,28 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use AppBundle\Service\MangoPayService;
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Routing\Router;
 
 class RegistrationListener implements EventSubscriberInterface
 {
     private $mangopayService;
     private $em;
+    private $router;
 
-    public function __construct(MangoPayService $mangopayService, EntityManager $em)
+    public function __construct(MangoPayService $mangopayService, EntityManager $em, Router $router)
     {
         $this->mangopayService = $mangopayService;
         $this->em = $em;
+        $this->router = $router;
     }
 
     public static function getSubscribedEvents()
     {
         return array(
             FOSUserEvents::REGISTRATION_COMPLETED => 'onUserRegistrationCompleted',
-            FOSUserEvents::REGISTRATION_CONFIRMED => 'onUserRegistrationConfirmed'
+            FOSUserEvents::REGISTRATION_CONFIRMED => 'onUserRegistrationConfirmed',
+            FOSUserEvents::REGISTRATION_SUCCESS => ['onUserRegistrationSuccess', -1]
         );
     }
 
@@ -54,6 +60,28 @@ class RegistrationListener implements EventSubscriberInterface
         // Flush user
         $this->em->persist($user);
         $this->em->flush();
+
+
+    }
+
+    public function onUserRegistrationSuccess(FormEvent $event)
+    {
+        $user = $event->getForm()->getData();
+
+        $response = new RedirectResponse(
+            $this->router->generate('homepage')
+        );
+
+        if ($user->getPro()) {
+            $response = new RedirectResponse(
+                $this->router->generate('sell_category')
+            );
+        }
+
+        var_dump($response);
+
+        $event->setResponse($response);
+
     }
 
     public function onUserRegistrationConfirmed(FilterUserResponseEvent $event) {
