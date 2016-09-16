c90b5eaa76337800849a0293cb6106312aaf70e1
jc <jeancharleslegoff@gmail.com>
2016-09-16 14:35:07 +0200

    Use callback transformer for meter and gram units

--- src/ProductBundle/Form/ProductType.php
+++ src/ProductBundle/Form/ProductType.php
@@ -7,6 +7,7 @@ use LocationBundle\Form\AddressType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
@@ -23,17 +24,42 @@ class ProductType extends AbstractType
             ->add('price', null, ['label' => 'Prix'])
             ->add('originalPrice', null, ['label' => 'Prix d\'origine'])
             ->add('allowOffer', null, ['label' => 'Autoriser l\'offre'])
-            ->add('weight', null, ['label' => 'Poids'])
-            ->add('width', null, ['label' => 'Largeur'])
-            ->add('length', null, ['label' => 'Longueur'])
-            ->add('height', null, ['label' => 'Hauteur'])
+            ->add('weight', null, ['label' => 'Poids en kg'])
+            ->add('width', null, ['label' => 'Largeur en cm'])
+            ->add('length', null, ['label' => 'Longueur en cm'])
+            ->add('height', null, ['label' => 'Hauteur en cm'])
             ->add('category', null, ['label' => 'Catégorie'])
             ->add('currency', null, ['label' => 'Devise'])
             ->add('status', null, ['label' => 'Statut'])
             ->add('address', AddressAdminType::class, ['label' => 'Adresse'])
         ;
+
+        // Transform weight in grams / kilograms
+        $builder->get('weight')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($weightToKiloGrams) {
+                    return $weightToKiloGrams / 1000;
+                },
+                function ($weightToGrams) {
+                    return $weightToGrams * 1000;
+                }
+            ));
+
+        // Transform width, length and height in meters / centimers
+        $metersConversionCallbackTransformer = new CallbackTransformer(
+            function ($valueToMeters) {
+                return $valueToMeters * 100;
+            },
+            function ($valueToCentimeters) {
+                return $valueToCentimeters / 100;
+            }
+        );
+        $builder->get('width')->addModelTransformer($metersConversionCallbackTransformer);
+        $builder->get('length')->addModelTransformer($metersConversionCallbackTransformer);
+        $builder->get('height')->addModelTransformer($metersConversionCallbackTransformer);
+
     }
-    
+
     /**
      * @param OptionsResolver $resolver
      */
