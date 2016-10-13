977f01e1683cec9d954e20224a001cabd7aac0e2
tdubuffet <dubuffet.thibault@gmail.com>
2016-10-13 14:23:29 +0200

    Fix redirection

--- src/UserBundle/Controller/SecurityController.php
+++ src/UserBundle/Controller/SecurityController.php
@@ -8,16 +8,4 @@ use FOS\UserBundle\Controller\SecurityController as BaseSecurityController;
 
 class SecurityController extends BaseSecurityController
 {
-
-    /**
-     * Overriding login to add custom logic.
-     */
-    public function loginAction(Request $request)
-    {
-        $securityContext = $this->container->get('security.authorization_checker');
-        if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            return new RedirectResponse($this->container->get('router')->generate('user_account_products'));
-        }
-        return parent::loginAction($request);
-    }
 }
