f9cd42ce9b6a8e99758529fb261da66a8d201781
tdubuffet <dubuffet.thibault@gmail.com>
2016-11-21 13:18:13 +0100

    Fix mixed content protocol

--- src/UserBundle/Resources/public/js/signup.js
+++ src/UserBundle/Resources/public/js/signup.js
@@ -125,7 +125,7 @@ var Signup = {
         $("input[name='fos_user_registration_form[phone]']").intlTelInput({
             initialCountry: "auto",
             geoIpLookup: function (callback) {
-                $.get('http://ipinfo.io', function () {
+                $.get('https://ipinfo.io', function () {
                 }, "jsonp").always(function (resp) {
                     var countryCode = (resp && resp.country) ? resp.country : "";
                     callback(countryCode);
